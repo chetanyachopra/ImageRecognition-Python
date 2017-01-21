f051c19026c694e2ab45e064219e4b55d9301534
chetanyachopra <chetanya221997chopra@gmail.com>
2017-01-21 21:14:06 +0530

    images added to compare the model

--- imgr.py
+++ imgr.py
@@ -2,56 +2,74 @@ import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
 import time
+
+def createExamples():
+    numberArrayExamples=open('numArEx.txt','a')
+    numbersWeHave=range(0,10)
+    versionsWeHave=range(1,10)
+
+    for eachNum in numbersWeHave:
+        for eachVer in versionsWeHave:
+             #print  str(eachNum)+'.'+str(versionsWeHave)
+             imageFilePath='images/numbers/'+str(eachNum)+'.'+str(eachVer)+'.png'
+             ei=Image.open(imageFilePath)
+             eiar=np.array(ei)
+             eiar1=str(eiar.tolist())
+             
+             lineToWrite=str(eachNum)+'::'+eiar1+'\n'
+             numberArrayExamples.write(lineToWrite)
+
+
 def threshold(imageArray):
     balanceAr = []
     newAr = imageArray
+
     for eachRow in imageArray:
         for eachPix in eachRow:
-            avgNum = reduce(lambda x, y: x + y, eachPix[:3]) / len(eachPix[:3])
+            avgNum=reduce(lambda x,y:x+y,eachPix[:3])/len(eachPix[:3])
             balanceAr.append(avgNum)
-    balance = reduce(lambda x, y: x + y, balanceAr) / len(balanceAr)
+    balance=reduce(lambda x,y:x+y,balanceAr)/len(balanceAr)
+
     for eachRow in newAr:
         for eachPix in eachRow:
-            if reduce(lambda x, y: x + y, eachPix[:3]) / len(eachPix[:3]) > balance:
-                eachPix[0] = 255
-                eachPix[1] = 255
-                eachPix[2] = 255
-                eachPix[3] = 255
+            if reduce(lambda x,y:x+y,eachPix[:3])/len(eachPix[:3]) > balance:
+                eachPix[0]=255
+                eachPix[1]=255
+                eachPix[2]=255
+                eachPix[3]=255
+
             else:
-                eachPix[0] = 0
-                eachPix[1] = 0
-                eachPix[2] = 0
-                eachPix[3] = 255
+                eachPix[0]=0
+                eachPix[1]=0
+                eachPix[2]=0
+                eachPix[3]=255
     return newAr
-
-
 i=Image.open('images/numbers/0.1.png')
-iar=np.asarray(i)
+iar=np.array(i)
+
 i2=Image.open('images/numbers/y0.4.png')
 iar2=np.array(i2)
-i3=Image.open('images/numbers/y0.4.png')
-iar3=np.asarray(i3)
-i4=Image.open('images/numbers/y0.4.png')
+
+i3=Image.open('images/numbers/y0.5.png')
+iar3=np.array(i3)
+
+i4=Image.open('images/sentdex.png')
 iar4=np.array(i4)
 
-iar = threshold(iar)
-iar2 = threshold(iar2)
-iar3 = threshold(iar3)
-iar4 = threshold(iar4)
-
+'''threshold(iar2)
+threshold(iar3)
+threshold(iar4)
 
 fig=plt.figure()
 ax1=plt.subplot2grid((8,6),(0,0),rowspan=4,colspan=3)
-ax2 = plt.subplot2grid((8,6),(4,0), rowspan=4, colspan=3)
-ax3 = plt.subplot2grid((8,6),(0,3), rowspan=4, colspan=3)
-ax4 = plt.subplot2grid((8,6),(4,3), rowspan=4, colspan=3)
-
-
+ax2=plt.subplot2grid((8,6),(4,0),rowspan=4,colspan=3)
+ax3=plt.subplot2grid((8,6),(0,3),rowspan=4,colspan=3)
+ax4=plt.subplot2grid((8,6),(4,3),rowspan=4,colspan=3)
 
 ax1.imshow(iar)
 ax2.imshow(iar2)
 ax3.imshow(iar3)
 ax4.imshow(iar4)
 
-
-plt.show()
+plt.show()'''
+  
