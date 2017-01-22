49c36e4519ff5ba84fc3c59d26a2566dd018d717
chetanyachopra <chetanya221997chopra@gmail.com>
2017-01-22 13:46:05 +0530

    basic pixel reading stuff done capable of recognizing basic numbers

--- imgr.py
+++ imgr.py
@@ -74,9 +74,31 @@ def whatNumIsThis(filepath):
 
 				x+=1;
 
-	print matchedArr
+	#print matchedArr
 	x=Counter(matchedArr)
 	print x
+        
+	graphX=[]
+	graphY=[]
 
-whatNumIsThis('images/numbers/2.2.png')
+	ylimi=0
+
+	for eachThing in x:
+		print eachThing
+		graphX.append(eachThing)
+		print x[eachThing]
+		graphY.append(x[eachThing])
+		ylimi=x[eachThing]
+
+	fig=plt.figure()
+	ax1=plt.subplot2grid((4,4),(0,0),rowspan=1,colspan=4)
+	ax2=plt.subplot2grid((4,4),(1,0),rowspan=3,colspan=4)
+	ax1.imshow(iar)
+	ax2.bar(graphX,graphY,align='center')
+	plt.ylim(400)
+	xloc=plt.MaxNLocator(12)
+	ax2.xaxis.set_major_locator(xloc)
+	plt.show()
+
+whatNumIsThis('images/numbers/6.1.png')
 					
